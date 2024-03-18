73a538bb0a2a1e19849ad70516f15940398ce22d
Saidumarov <saidumarovjafarxon@gmail.com>
2024-03-18 11:14:54 -0300

    .

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -8,8 +8,9 @@ const Carousel = () => {
     dots: true,
     infinite: true,
     speed: 400,
-    slidesToShow: 2.1,
+    slidesToShow: 2.2,
     slidesToScroll: 1,
+    variableWidth: true,
   };
   return (
     <>
@@ -30,12 +31,111 @@ const Carousel = () => {
               <Slider {...settings}>
                 <div className="carusel_item">
                   <img src={img} alt="" />
+                  <div className="hover">
+                    <p className="hover_p">
+                      01
+                      <svg
+                        width="27"
+                        height="2"
+                        viewBox="0 0 27 2"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M0 1H27" stroke="#616161" />
+                      </svg>
+                      Bed Room
+                    </p>
+                    <h3>Inner Peace</h3>
+                    <div className="next">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M21 12H3M21 12L15 6M21 12L15 18"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
                 <div className="carusel_item">
                   <img src={img1} alt="" />
+                  <div className="hover">
+                    <p className="hover_p">
+                      01
+                      <svg
+                        width="27"
+                        height="2"
+                        viewBox="0 0 27 2"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M0 1H27" stroke="#616161" />
+                      </svg>
+                      Bed Room
+                    </p>
+                    <h3>Inner Peace</h3>
+                    <div className="next">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M21 12H3M21 12L15 6M21 12L15 18"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
                 <div className="carusel_item">
                   <img src={img2} alt="" />
+                  <div className="hover">
+                    <p className="hover_p">
+                      01
+                      <svg
+                        width="27"
+                        height="2"
+                        viewBox="0 0 27 2"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M0 1H27" stroke="#616161" />
+                      </svg>
+                      Bed Room
+                    </p>
+                    <h3>Inner Peace</h3>
+                    <div className="next">
+                      <svg
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M21 12H3M21 12L15 6M21 12L15 18"
+                          stroke="white"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
               </Slider>
             </div>
